9673aee169a2145f73228f70fcbb8c879a0bd82a
Bill McAlister <57473785+bill-mca@users.noreply.github.com>
2023-06-02 19:38:01 +1000

    Upload of old code
    downloaded code from the webhost for this commit. The hillshade tiles are missing and will need to be regenerated.

---
+++ jsons/pois.js
@@ -0,0 +1,21 @@
+var pois = {
+"type": "FeatureCollection",
+"name": "pois",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": 1, "description": "secondary channel perched above an incised primary" }, "geometry": { "type": "Point", "coordinates": [ 150.15035, -33.06897 ] } },
+{ "type": "Feature", "properties": { "fid": 2, "description": "floodout" }, "geometry": { "type": "Point", "coordinates": [ 150.14321, -33.07223 ] } },
+{ "type": "Feature", "properties": { "fid": 3, "description": "headcuts" }, "geometry": { "type": "Point", "coordinates": [ 150.14607, -33.07081 ] } },
+{ "type": "Feature", "properties": { "fid": 4, "description": "incised secondary channel" }, "geometry": { "type": "Point", "coordinates": [ 150.14189, -33.07308 ] } },
+{ "type": "Feature", "properties": { "fid": 5, "description": "secondary channel perched above an incised primary" }, "geometry": { "type": "Point", "coordinates": [ 150.10098, -33.08602 ] } },
+{ "type": "Feature", "properties": { "fid": 6, "description": "upper limit of deposition" }, "geometry": { "type": "Point", "coordinates": [ 150.08678, -33.09805 ] } },
+{ "type": "Feature", "properties": { "fid": 7, "description": "upper limit of deposition]" }, "geometry": { "type": "Point", "coordinates": [ 150.09316, -33.09483 ] } },
+{ "type": "Feature", "properties": { "fid": 8, "description": "upper limit of deposition" }, "geometry": { "type": "Point", "coordinates": [ 150.08257, -33.09556 ] } },
+{ "type": "Feature", "properties": { "fid": 9, "description": "upper limit of deposition" }, "geometry": { "type": "Point", "coordinates": [ 150.08639, -33.0927 ] } },
+{ "type": "Feature", "properties": { "fid": 10, "description": "upper limit of deposition" }, "geometry": { "type": "Point", "coordinates": [ 150.09678, -33.09569 ] } },
+{ "type": "Feature", "properties": { "fid": 11, "description": "upper limit of deposition" }, "geometry": { "type": "Point", "coordinates": [ 150.09773, -33.09461 ] } },
+{ "type": "Feature", "properties": { "fid": 12, "description": "discontinuous incision" }, "geometry": { "type": "Point", "coordinates": [ 150.10947, -33.09445 ] } },
+{ "type": "Feature", "properties": { "fid": 13, "description": "multiple terrace levels" }, "geometry": { "type": "Point", "coordinates": [ 150.12869, -33.08074 ] } },
+{ "type": "Feature", "properties": { "fid": 14, "description": "confluence" }, "geometry": { "type": "Point", "coordinates": [ 150.17305, -33.06072 ] } }
+]
+}
